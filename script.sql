5701edda53be3520d1eb4a3d25b17325d02c110b
Francelino Augusto <87836783+Francelino12@users.noreply.github.com>
2023-11-02 01:45:35 +0000

    AULA 01-11-2023

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+-- Active: 1698887756949@@127.0.0.1@3306@wpdb
 -- Exibir os bancos de dados do servidor
 # Comentário (MySQL)
 /* Comentário de várias linhas (MySQL) */
@@ -8,3 +9,24 @@ SHOW TABLES FROM information_schema;
 
 -- Criaçao de nova base de dados
 CREATE DATABASE banco_de_teste;
+
+-- Remove uma base de dados
+
+DROP DATABASE banco_de_teste;
+
+-- Mostra o nome de cada database
+CREATE TABLE banco_de_teste.alunos(
+    id INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+SHOW TABLES FROM banco_de_teste;
+
+SHOW FIELDS FROM banco_de_teste.alunos;
+
+USE banco_de_teste;
+
+CREATE TABLE teste(
+    id INT PRIMARY KEY,
+    valor CHAR (11)
+);
